d32a8c516b9f2f7d91748e761ae6b48932308a8c
ldbl <stanfold@gmail.com>
2026-02-20 11:57:14 +0200

    use kubernetes provider

--- infra/terraform/hcloud_cluster/main.tf
+++ infra/terraform/hcloud_cluster/main.tf
@@ -130,12 +130,18 @@ resource "local_sensitive_file" "kubeconfig" {
 
 provider "helm" {
   kubernetes {
-    config_path = fileexists(local.kubeconfig_path) ? local.kubeconfig_path : "/dev/null"
+    host                   = module.kube_hetzner.kubeconfig_data.host
+    client_certificate     = module.kube_hetzner.kubeconfig_data.client_certificate
+    client_key             = module.kube_hetzner.kubeconfig_data.client_key
+    cluster_ca_certificate = module.kube_hetzner.kubeconfig_data.cluster_ca_certificate
   }
 }
 
 provider "kubernetes" {
-  config_path = fileexists(local.kubeconfig_path) ? local.kubeconfig_path : "/dev/null"
+  host                   = module.kube_hetzner.kubeconfig_data.host
+  client_certificate     = module.kube_hetzner.kubeconfig_data.client_certificate
+  client_key             = module.kube_hetzner.kubeconfig_data.client_key
+  cluster_ca_certificate = module.kube_hetzner.kubeconfig_data.cluster_ca_certificate
 }
 
 resource "kubernetes_namespace" "bootstrap" {
